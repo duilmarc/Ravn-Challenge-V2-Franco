5f65fe3f0b1a7f28012428d46a65a2cab71195f9
duilmarc <franco.chp@gmail.com>
2020-12-21 01:27:36 -0500

    build: Componet Character detaill Info

--- rvn-challenge/src/components/CharacterInfo/CharacterInfo.js
+++ rvn-challenge/src/components/CharacterInfo/CharacterInfo.js
@@ -1,12 +1,52 @@
+import './Characterinfo.css';
 import React from 'react'
-import {render} from 'react-dom'
-import "./CharacterInfo.css"
+import {withRouter} from 'react-router-dom';
+import { DETAILLINFO } from './../../Apollo-Client/query'
+import { useQuery } from '@apollo/client';
 
-class CharacterInfo extends React.Component{
+function UpperCaseFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
-
+const INFO = ({header, text}) => {
+    return (
+      <div className='info-content'>
+        <h2 className="header">{header}</h2>
+        <h2 className="text">{UpperCaseFirstLetter(text)}</h2>
+        <hr />
+      </div>
+    )
 }
 
-render
+const CharacterInfo = ({match:{params:{id}}}) => {
+    const { data, loading, error } = useQuery(DETAILLINFO,{variables : {id : id }});
+    if(error) return <p></p>;
+    if( loading) return <p></p>;
+    const detaillInfo = data.person 
+    return(
+        <>
+            <div className="container-detail">
+                <div className="title">General Information</div>
+                <INFO header={"Eye Color"} text={detaillInfo.eyeColor}/>
+                <INFO header={"Hair Color"} text={detaillInfo.hairColor}/>
+                <INFO header={"Skin Color"} text={detaillInfo.skinColor}/>
+                <INFO header={"Birth Year"} text={detaillInfo.birthYear}/>
+                {
+                    detaillInfo.vehicleConnection.vehicles.length > 0 ? 
+                    <div className="title">Vehicles</div>
+                    : 
+                    <div className="title">No Vehicles Found </div>
+                }
+                {
+                    detaillInfo.vehicleConnection.vehicles.map((vehicle) => (
+                        <INFO key={vehicle.name} header={vehicle.name} text={''}/>
+                    ))
+                }
+            </div>
+        </>
+    )
+    
+    
+}
 
-export default CharacterInfo
+export default withRouter(CharacterInfo);
--- rvn-challenge/src/components/CharacterList/CharacterList.js
+++ rvn-challenge/src/components/CharacterList/CharacterList.js
@@ -1,6 +1,8 @@
+import './CharacterList.css'
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 import { Row, Col } from 'react-bootstrap';
+import { AiOutlineRight } from "react-icons/ai";
 
 class CharacterList extends Component {
   render() {
@@ -12,10 +14,19 @@ class CharacterList extends Component {
             return (
               <Link className="Reference" key={idx} to={`/${node.id}`}>
                 <div className="Character">
-                  <h2>
-                    {node.name}
-                  </h2>
-                  <p>{node.species === null ? "Human" : node.species.name } from {node.homeworld.name}</p>
+                <Row>
+                  <Col xs={10} >
+                      <h2>
+                        {node.name}
+                      </h2>
+                      <p>{node.species === null ? "Human" : node.species.name } from {node.homeworld.name}</p>
+                  </Col>
+                  <Col xs={2}>
+                    <div className="Icon">
+                    <AiOutlineRight />
+                    </div>
+                  </Col>
+                </Row>
                 </div>
                 <hr/>
               </Link>
